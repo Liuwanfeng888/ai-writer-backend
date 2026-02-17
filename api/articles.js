e3fd4f18c32daebae57b0410dfbc099a08342f68
Liuwanfeng888 <wanfengliu888@gmail.com>
2026-02-17 17:16:24 +0800

    Update articles.js

--- api/articles.js
+++ api/articles.js
@@ -2,6 +2,13 @@ import { supabase } from '../utils/supabase.js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   
   if (req.method === 'OPTIONS') {
